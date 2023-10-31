28418fdb6ba20432bb390acf44457c83f7c9ab85
carolinacc2323 <carolinacc.tandem@gmail.com>
2023-10-31 13:17:00 +0100

    primercommit

---
+++ bucle-foreach.js
@@ -0,0 +1,8 @@
+const personas=['Kyllian','Cris','Dani'];
+personas.forEach(function (personas,index) {
+    console.log(index);
+    console.log(personas);
+});
+personas.forEach((persona,index)=>{
+    console.log(persona);
+});
